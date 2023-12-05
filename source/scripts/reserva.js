49b75a29ccc14fc2c4184e895376eedd2c951ede
100472360 <100472360@alumnos.uc3m.es>
2023-12-05 12:59:27 +0100

    selectores

--- source/scripts/reserva.js
+++ source/scripts/reserva.js
@@ -1,11 +1,3 @@
-$(document).ready(function(){
-    $(".panel-fecha").hide();
-    $("#fecha").click(function(){
-      $(".panel-fecha").slideToggle("slow");
-    });
-});
-
-
 $(".readonly").keydown(function(e) {
   e.preventDefault();
 });
